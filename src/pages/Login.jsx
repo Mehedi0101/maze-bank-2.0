eb06532185e056f2a8568dd7b73d2c9eabaf44ca
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-29 09:39:59 +0600

    contents added in login and register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleRegister = () => navigate('/register');
+
     return (
-        <div>
-            
+        <div className="h-[80vh] flex justify-center items-center">
+            <form className="flex flex-col space-y-2">
+                <h2 className="text-2xl font-semibold text-red-700">Log into your account</h2>
+                <input className="outline-none bg-slate-200 px-3 py-2 rounded-lg" type="email" name="email" placeholder="Email" id="" />
+                <div className='flex relative'>
+                    <input className="outline-none bg-slate-200 px-3 py-2 rounded-lg w-full" type={showPassword ? 'text' : 'password'} name="password" placeholder="Password" id="" />
+                    <div onClick={() => setShowPassword(!showPassword)} className='absolute right-2 flex items-center h-full text-red-700 cursor-pointer'>
+                        {showPassword ? <BsEyeSlashFill /> : <BsEyeFill />}
+                    </div>
+                </div>
+                <input className="outline-none bg-red-700 text-white font-semibold text-lg px-5 py-2 rounded-lg cursor-pointer active:scale-95 transition-transform" type="submit" value="Login" />
+                <div className="text-red-700 font-medium">
+                    <p>Don&apos;t have an account?</p>
+                    <p onClick={handleRegister} className="text-blue-700 underline font-semibold cursor-pointer">Register</p>
+                </div>
+            </form>
         </div>
-    );
+    )
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import { BsEyeFill } from 'react-icons/bs';
+import { BsEyeSlashFill } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+
 const Register = () => {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleLogin = () => navigate('/login');
+
     return (
-        <div>
-            
+        <div className="h-[80vh] flex justify-center items-center">
+            <form className="flex flex-col space-y-2">
+                <h2 className="text-2xl font-semibold text-red-700">Create a new account</h2>
+                <input className="outline-none bg-slate-200 px-3 py-2 rounded-lg" type="text" name="name" placeholder="Name" />
+                <input className="outline-none bg-slate-200 px-3 py-2 rounded-lg" type="email" name="email" placeholder="Email" id="" />
+                <div className='flex relative'>
+                    <input className="outline-none bg-slate-200 px-3 py-2 rounded-lg w-full" type={showPassword ? 'text' : 'password'} name="password" placeholder="Password" id="" />
+                    <div onClick={() => setShowPassword(!showPassword)} className='absolute right-2 flex items-center h-full text-red-700 cursor-pointer'>
+                        {showPassword ? <BsEyeSlashFill /> : <BsEyeFill />}
+                    </div>
+                </div>
+                <input className="outline-none bg-red-700 text-white font-semibold text-lg px-5 py-2 rounded-lg cursor-pointer active:scale-95 transition-transform" type="submit" value="Register" />
+                <div className="text-red-700 font-medium">
+                    <p>Already have an account?</p>
+                    <p onClick={handleLogin} className="text-blue-700 underline font-semibold cursor-pointer">login</p>
+                </div>
+            </form>
         </div>
     );
 };
